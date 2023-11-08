1a3c6456ba7da02aef1cc305958891bb3ad43023
jakubmanczak <jakub@manczak.net>
2023-11-08 20:16:00 +0100

    server receive messages from multiple connections via forking the process

--- src/main.c
+++ src/main.c
@@ -10,12 +10,22 @@
 
 #define PORT "2004"
 #define BACKLOG 8
+#define MAXCLIENTS 32
+#define MSGBUFFER 256
+#define NAMEBUFFER 32
+#define LOGBUFFER (MSGBUFFER + NAMEBUFFER + 32)
 
-char *welcomemsg = "You're connected to picochat.\n";
+typedef struct client{
+  int connfd;
+  // char *name;
+} client;
 
-int main(void){
-  int sockfd; struct addrinfo hints, *res; int yes = 1;
+char *welcomemsg = "You've reached a picochat server.\n";
+int sockfd; int clients = 0;
 
+void setupServerNetworking(){
+  struct addrinfo hints, *res; int yes = -1;
+  
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC; hints.ai_socktype = SOCK_STREAM; hints.ai_flags = AI_PASSIVE;
   getaddrinfo(NULL, PORT, &hints, &res);
@@ -23,33 +33,39 @@ int main(void){
   bind(sockfd, res->ai_addr, res->ai_addrlen);
   setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
   listen(sockfd, BACKLOG);
+}
 
-  int connfd = 0;
-  int buffersize = 256;
-  char *buffer = malloc(buffersize * sizeof(char));
+int main(void){
+  setupServerNetworking();
 
   while(1){
-    if(!connfd){
-      struct sockaddr_storage connstore;
-      socklen_t addrsize;
+    client cl; cl.connfd = 0;
 
-      addrsize = sizeof connstore;
-      connfd = accept(sockfd, (struct sockaddr *)&connstore, &addrsize);
-      send(connfd, welcomemsg, strlen(welcomemsg), 0);
-      printf("Connection (%d) accepted.\n", connfd);
-    }else{
-      memset(buffer, 0, buffersize);
-      int rv = recv(connfd, buffer, buffersize, 0);
-      if(rv == 0){
-        close(connfd); printf("Connection (%d) closed.\n", connfd); connfd = 0;
-      }else{
-        printf("%d says: %s", connfd, buffer);
-        send(connfd, "received message:\n", 19, 0);
-        send(connfd, buffer, strlen(buffer), 0);
-        send(connfd, "\n", 2, 0);
+    struct sockaddr_storage connstore;
+    socklen_t addrsize;
+
+    addrsize = sizeof connstore;
+    cl.connfd = accept(sockfd, (struct sockaddr *)&connstore, &addrsize);
+    printf("conn fd:%d accepted\n", cl.connfd);
+    if(!fork()){
+      char *buffer = malloc(MSGBUFFER); int rv = 0xbeef;
+      close(sockfd); clients++;
+      while(1){
+        rv = recv(cl.connfd, buffer, MSGBUFFER, 0);
+        if(rv > 0){
+          printf("%d: %s", cl.connfd, buffer);
+          memset(buffer, 0, MSGBUFFER);
+        }else if(rv == 0){
+          printf("conn fd:%d dropped\n", cl.connfd);
+          close(cl.connfd); free(buffer);
+          exit(0);
+        }else{
+          // error occured
+          printf("recv() error!\n");
+          exit(1);
+        }
       }
     }
   }
-
   return 0;
 }
