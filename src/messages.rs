1105c9eda8756dda1bab8e507da9afc7cc00adbc
jakubmanczak <jakub@manczak.net>
2025-02-20 11:37:42 +0100

    nickname picking work (can't be empty, must contain letter, buffer flush)

--- src/messages.rs
+++ src/messages.rs
@@ -1,7 +1,4 @@
 pub const REACHED: &str = "You've reached a picochat server.\n";
-pub const PICK_NAME: &str = "Please pick a nickname (ASCII alphanumerics/dots/hyphens only): ";
-pub const CHOSEN_NAME: &str = "You have chosen the nickname: REPL\n";
-pub const NAME_IN_USE: &str = "This name is already in use.\n";
 pub const WELCOME: &str = "You are now connected to the chat server. Messages starting with / are commands and won't be broadcast. Type /help for more info.\n";
 pub const USERS_ONLINE: &str = "Users online: REPL.\n";
 pub const NO_USERS_ONLINE: &str = "No users online.\n";
--- src/routines.rs
+++ src/routines.rs
@@ -5,7 +5,7 @@ use tokio::{
 
 use crate::{
     broadcasts::Broadcast,
-    messages::{CHOSEN_NAME, PICK_NAME, REACHED, WELCOME},
+    messages::{REACHED, WELCOME},
     state::{ServerState, User},
 };
 
@@ -21,18 +21,35 @@ pub async fn postjoin(socket: &mut TcpStream) -> io::Result<()> {
     Ok(())
 }
 
+const PICK_NAME: &[u8] = b"Please pick a nickname (ASCII alphanumerics/dots/hyphens only): ";
+const NAME_NOT_EMPTY: &[u8] = b"Nickname must not be empty.\n";
+const NAME_NOT_PUNCTUATION: &[u8] = b"Nickname must include some letters\n";
+const NAME_IN_USE: &[u8] = b"This nickname is already in use.\n";
+const CHOSEN_NAME: &str = "You have chosen the nickname: REPL\n";
+
 pub async fn get_nickname(socket: &mut TcpStream, state: &ServerState) -> io::Result<Option<User>> {
     let mut buffer = [0u8; 12];
     loop {
-        socket.write_all(PICK_NAME.as_bytes()).await?;
+        socket.write_all(PICK_NAME).await?;
         match socket.read(&mut buffer).await {
             Ok(0) | Err(_) => return Ok(None),
             Ok(_) => (),
         };
+
         let name: String = String::from_utf8_lossy(&buffer)
             .chars()
             .filter(|c| c.is_ascii_alphanumeric() || *c == '-' || *c == '.')
             .collect();
+
+        if name.len() == 0 {
+            socket.write_all(NAME_NOT_EMPTY).await?;
+            continue;
+        }
+        if !name.chars().any(|c| c.is_alphabetic()) {
+            socket.write_all(NAME_NOT_PUNCTUATION).await?;
+            continue;
+        }
+
         let msg = CHOSEN_NAME.replace("REPL", &name);
         socket.write_all(msg.as_bytes()).await?;
 
@@ -47,5 +64,7 @@ pub async fn get_nickname(socket: &mut TcpStream, state: &ServerState) -> io::Re
                 .unwrap();
             return Ok(Some(user));
         }
+        socket.write_all(NAME_IN_USE).await?;
+        buffer.fill(0);
     }
 }
